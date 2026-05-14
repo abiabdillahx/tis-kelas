8d072434ac8e0331ef31aa40582d3e440e82fb61
Abi <83850574+abiabdillahx@users.noreply.github.com>
2026-05-14 14:03:17 +0700

    refactor: modul 6

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'dummy.jwt' => \App\Http\Middleware\DummyJwtMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,41 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ping', function () {
-    return response()->json(['message' => 'pong']);
+Route::get("/ping", function () {
+    return response()->json(["message" => "pong"]);
 });
 
-Route::get('/status', function () {
+Route::get("/status", function () {
     return response()->json([
         "app" => "Todo API",
-        "status" => "running"
+        "status" => "running",
     ]);
 });
 
 // CRUD Student
-Route::post('/students', [StudentController::class, 'store']);
-Route::get('/students', [StudentController::class, 'index']);
-Route::get('/students/search', [StudentController::class, 'search']);
+Route::post("/students", [StudentController::class, "store"]);
+Route::get("/students", [StudentController::class, "index"]);
+Route::get("/students/search", [StudentController::class, "search"]);
 
-Route::get('/students/{nim}/mata-kuliah', [StudentController::class, 'mataKuliahByStudent']);
-Route::get('/students/{nim}', [StudentController::class, 'show']);
+Route::get("/students/{nim}/mata-kuliah", [
+    StudentController::class,
+    "mataKuliahByStudent",
+]);
+Route::get("/students/{nim}", [StudentController::class, "show"]);
 
-Route::put('/students/{nim}', [StudentController::class, 'update']);
-Route::patch('/students/{nim}', [StudentController::class, 'update']);
-Route::delete('/students/{nim}', [StudentController::class, 'destroy']);
+Route::put("/students/{nim}", [StudentController::class, "update"]);
+Route::patch("/students/{nim}", [StudentController::class, "update"]);
+Route::delete("/students/{nim}", [StudentController::class, "destroy"]);
+
+// auth
+Route::post("/register", [AuthController::class, "register"]);
+Route::post("/login", [AuthController::class, "login"]);
+Route::middleware(["dummy.jwt"])->group(function () {
+    Route::post("/logout", [AuthController::class, "logout"]);
+    Route::get("/profile", [AuthController::class, "profile"]);
+    Route::get("/token-check", [AuthController::class, "tokenCheck"]);
+});
